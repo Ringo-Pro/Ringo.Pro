4244a1b5b8fe081913d8302c63ab5ac6e800e30c
Marten de Bruijn <martenbruijn@gmail.com>
2020-05-25 16:28:57 +0200

    changes

--- server/app.js
+++ server/app.js
@@ -1,16 +1,14 @@
 require('dotenv').config();
 const router = require('./routes/router.js'),
-  api = require('./apis/genius.js'),
   port = process.env.PORT || 4000,
   express = require('express'),
   bodyParser = require('body-parser'),
   path = require('path'),
-  app = express()
-  fetch = require('node-fetch')
-  cors = require('cors')
-  querystring = require('querystring')
-  cookieParser = require('cookie-parser')
-  genius = require('./apis/genius.js')
+  app = express();
+fetch = require('node-fetch');
+cors = require('cors');
+querystring = require('querystring');
+cookieParser = require('cookie-parser');
 const { URLSearchParams } = require('url');
 
 /**
@@ -19,15 +17,16 @@ const { URLSearchParams } = require('url');
  * @return {string} The generated string
  */
 const generateRandomString = (length) => {
-    var text = '';
-    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  
-    for (var i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  };
-  
+  var text = '';
+  var possible =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
 let stateKey = 'spotify_auth_state';
 
 app
@@ -35,11 +34,9 @@ app
   .use(bodyParser.urlencoded({ extended: true }))
   .use(cors())
   .use(cookieParser())
-  .set('views', __dirname + '/view/pages')
+  .set('view', __dirname + '/view/pages')
   .set('view engine', 'ejs')
   .get('/', homeRoute)
-  .get('/genius-auth', router.geniusRedirect)
-  .get('/search', router.search)
   .get('/callback', callback)
   .get('/searchResults', searchResultsRoute)
   .get('/track/:id/:token', detailRoute)
@@ -49,33 +46,31 @@ app.listen(port, () => {
   console.log(`Dev app listening on port: ${port}`);
 });
 
+function callback(req, res) {
+  let code = req.query.code || null;
+  let state = req.query.state || null;
+  let storedState = req.cookies ? req.cookies[stateKey] : null;
 
-function callback(req, res){
-
-    let code = req.query.code || null
-    let state = req.query.state || null
-    let storedState = req.cookies ? req.cookies[stateKey] : null
-    
-    
-
-    if(state === null || state !== storedState){
-        res.redirect('/' + 
+  if (state === null || state !== storedState) {
+    res.redirect(
+      '/' +
         querystring.stringify({
-            error: 'state_mismatch'
-        }))
-    } else {
-        res.clearCookie(stateKey)
+          error: 'state_mismatch',
+        })
+    );
+  } else {
+    res.clearCookie(stateKey);
 
     let form = {
-        code: code,
-        redirect_uri: process.env.REDIRECT_URI,
-        grant_type: 'authorization_code'
-    }
+      code: code,
+      redirect_uri: process.env.REDIRECT_URI,
+      grant_type: 'authorization_code',
+    };
 
     const params = new URLSearchParams();
 
-    for(key in form){
-        params.append(key, form[key])
+    for (key in form) {
+      params.append(key, form[key]);
     }
 
     // const spotifyToken = Api.getSpotifyToken(params)
@@ -83,182 +78,185 @@ function callback(req, res){
     // console.log('SpotifyToken Function Module: ', spotifyToken)
 
     fetch(`https://accounts.spotify.com/api/token`, {
-        method: 'post',
-        headers: {
-            'Authorization': 'Basic ' + (new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'))
-        },
-        body: params,
-        'Content-Type':'application/x-www-form-urlencoded'
+      method: 'post',
+      headers: {
+        Authorization:
+          'Basic ' +
+          new Buffer.from(
+            process.env.SPOTIFY_CLIENT_ID +
+              ':' +
+              process.env.SPOTIFY_CLIENT_SECRET
+          ).toString('base64'),
+      },
+      body: params,
+      'Content-Type': 'application/x-www-form-urlencoded',
     })
-        .then(res => res.json())
-        .then(body => {
-            let access_token = body.access_token
-            let refresh_token = body.refresh_token
+      .then((res) => res.json())
+      .then((body) => {
+        let access_token = body.access_token;
+        let refresh_token = body.refresh_token;
 
-            let options = {
-                headers: { 'Authorization': 'Bearer ' + access_token },
-            };
+        let options = {
+          headers: { Authorization: 'Bearer ' + access_token },
+        };
 
         // Api.getSpotifyUserInfo(options)
 
-            fetch(`https://api.spotify.com/v1/me`, options)
-                .then(res => {
-                    if(!res.ok) {
-                    
-                        res.redirect('/' + 
-                            querystring.stringify({
-                            error: 'invalid_token'
-                        }))
-                    } else{
-                        return res.json()
-                    }
-                })
-                .then(body => {
-                    res.render('logged-in', {
-                    data: body,
-                    token: access_token
-                    })
-                })
-                .catch(err => {
-                    throw Error(err);
-                })
-        })
-    }
-
+        fetch(`https://api.spotify.com/v1/me`, options)
+          .then((res) => {
+            if (!res.ok) {
+              res.redirect(
+                '/' +
+                  querystring.stringify({
+                    error: 'invalid_token',
+                  })
+              );
+            } else {
+              return res.json();
+            }
+          })
+          .then((body) => {
+            res.render('logged-in', {
+              data: body,
+              token: access_token,
+            });
+          })
+          .catch((err) => {
+            throw Error(err);
+          });
+      });
+  }
 }
 
-function searchResultsRoute(req, res){
+function searchResultsRoute(req, res) {
+  console.log(req.query);
 
-    console.log(req.query)
+  let artist = req.query.searchValue;
+  let access_token = req.query.token;
+  let userData = JSON.parse(req.query.data);
 
-    let artist = req.query.searchValue
-    let access_token = req.query.token
-    let userData = JSON.parse(req.query.data)
+  let options = {
+    // url: `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
+    method: 'GET',
+    headers: { Authorization: 'Bearer ' + access_token },
+  };
 
-    let options = {
-        // url: `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + access_token },
-        
-    };
-    
-    fetch(`https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`, options)
-        .then(res => res.json())
-        .then(body => {
+  fetch(
+    `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
+    options
+  )
+    .then((res) => res.json())
+    .then((body) => {
+      console.log(body.tracks.items[0].album.images);
 
-            console.log(body.tracks.items[0].album.images)
-
-            res.render("search-results", {
-                trackData: body.tracks.items,
-                data: userData,
-                token: access_token
-            })
-
-        })     
+      res.render('search-results', {
+        trackData: body.tracks.items,
+        data: userData,
+        token: access_token,
+      });
+    });
 }
 
-function detailRoute(req, res){
+function detailRoute(req, res) {
+  console.log(req.params);
 
-    console.log(req.params)
+  const trackId = req.params.id.substring(1);
+  const access_token = req.params.token.substring(1);
 
-    const trackId = req.params.id.substring(1)
-    const access_token = req.params.token.substring(1)
+  console.log(access_token);
+  console.log(trackId);
 
-    console.log(access_token)
-    console.log(trackId)
+  let options = {
+    // url: `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
+    method: 'GET',
+    headers: { Authorization: 'Bearer ' + access_token },
+  };
 
-    let options = {
-        // url: `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + access_token },
-        
-    };
-
-
-    fetch(`https://api.spotify.com/v1/audio-features/${trackId}`, options)
-        .then(res => res.json())
-        .then(body => {
-            console.log(body)
-            res.render('track-detail', {
-                data: body
-            })
-        })
+  fetch(`https://api.spotify.com/v1/audio-features/${trackId}`, options)
+    .then((res) => res.json())
+    .then((body) => {
+      console.log(body);
+      res.render('track-detail', {
+        data: body,
+      });
+    });
 }
 
-function inspireMe(req, res){
+function inspireMe(req, res) {
+  let userData = JSON.parse(req.query.data);
+  const valence = req.query.valence;
+  const energy = req.query.energy;
+  const danceability = req.query.danceability;
+  const acousticness = req.query.acousticness;
+  const access_token = req.query.token;
+  const genres = req.query.genre;
+  let genreQuery;
 
-    let userData = JSON.parse(req.query.data)
-    const valence = req.query.valence
-    const energy = req.query.energy
-    const danceability = req.query.danceability
-    const acousticness = req.query.acousticness
-    const access_token = req.query.token
-    const genres = req.query.genre
-    let genreQuery
+  let options = {
+    // url: `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
+    method: 'GET',
+    headers: { Authorization: 'Bearer ' + access_token },
+  };
 
-    let options = {
-        // url: `https://api.spotify.com/v1/search?q=${artist}&type=track%2Cartist&market=US&limit=10&offset=5`,
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + access_token },
-        
-    };
+  if (Array.isArray(genres)) {
+    // genres.pop()
 
-    
+    // const lastGenre = genres.pop()
 
-    if(Array.isArray(genres)){
-        // genres.pop()
-        
-        // const lastGenre = genres.pop()
+    const lastElement = genres.splice(-1, 1);
 
-        const lastElement = genres.splice(-1,1)
+    let chainedArray = genres.map((genre) => (genre = `${genre}%2C`));
 
+    genreQuery = chainedArray.join('') + lastElement;
 
-        let chainedArray = genres.map(genre => genre = `${genre}%2C`)
+    fetch(
+      `https://api.spotify.com/v1/recommendations?limit=20&market=US&target_acousticness=${acousticness}&target_danceability=${danceability}&target_energy=${energy}&target_valence=${valence}&seed_genres=${genreQuery}`,
+      options
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        console.log(body.tracks);
 
-        genreQuery = chainedArray.join("") + lastElement
+        res.render('search-results', {
+          trackData: body.tracks,
+          data: userData,
+          token: access_token,
+        });
+      });
+  } else {
+    genreQuery = genres;
 
-        fetch(`https://api.spotify.com/v1/recommendations?limit=20&market=US&target_acousticness=${acousticness}&target_danceability=${danceability}&target_energy=${energy}&target_valence=${valence}&seed_genres=${genreQuery}`, options)
-            .then(res => res.json())
-            .then(body => {
-
-                console.log(body.tracks)
-
-                res.render("search-results", {
-                    trackData: body.tracks,
-                    data: userData,
-                    token: access_token
-                })
-            })
-    } else {
-        genreQuery = genres
-
-        fetch(`https://api.spotify.com/v1/recommendations?limit=20&market=US&target_acousticness=${acousticness}&target_danceability=${danceability}&target_energy=${energy}&target_valence=${valence}&seed_genres=${genreQuery}`, options)
-            .then(res => res.json())
-            .then(body => {
-                res.render("search-results", {
-                    trackData: body.tracks,
-                    data: userData,
-                    token: access_token
-                })
-            })
-    }
-
+    fetch(
+      `https://api.spotify.com/v1/recommendations?limit=20&market=US&target_acousticness=${acousticness}&target_danceability=${danceability}&target_energy=${energy}&target_valence=${valence}&seed_genres=${genreQuery}`,
+      options
+    )
+      .then((res) => res.json())
+      .then((body) => {
+        res.render('search-results', {
+          trackData: body.tracks,
+          data: userData,
+          token: access_token,
+        });
+      });
+  }
 }
 
-function homeRoute(req, res){
-    let state = generateRandomString(16)
+function homeRoute(req, res) {
+  let state = generateRandomString(16);
 
-    res.cookie(stateKey, state)
-    
-    const scopes = 'user-read-private user-read-email';
-    // const redirect_uri = process.env.REDIRECT_URI;
+  res.cookie(stateKey, state);
 
-    res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
+  const scopes = 'user-read-private user-read-email';
+  // const redirect_uri = process.env.REDIRECT_URI;
+
+  res.redirect(
+    'https://accounts.spotify.com/authorize?' +
+      querystring.stringify({
         response_type: 'code',
-        client_id: process.env.CLIENT_ID,
+        client_id: process.env.SPOTIFY_CLIENT_ID,
         scope: scopes,
         redirect_uri: process.env.REDIRECT_URI,
-        state: state
-    }));
+        state: state,
+      })
+  );
 }
--- server/routes/router.js
+++ server/routes/router.js
@@ -1,36 +1,7 @@
-const genius = require('../apis/genius.js');
-const musixmatch = require('../apis/musixmatch.js');
-
 const homeRoute = (req, res) => {
   res.render('home', {
     title: 'home',
-    genius: genius.authURL,
   });
 };
-function geniusRedirect(req, res) {
-  const code = req.query.code;
-  genius.getAccesToken(code);
-  let results = '';
-  res.render('search', {
-    title: 'search',
-    results: results,
-  });
-}
-async function search(req, res) {
-  // const _test = await musixmatch.getLyrics();
-  const searchQuery = req.query.search;
-  let results = '';
-  if (searchQuery) {
-    const regex = /\s/g;
-    const queryWithoutSpaces = searchQuery.replace(regex, '+');
-    results = await genius.search(queryWithoutSpaces);
-    console.log(results);
-  }
 
-  res.render('search', {
-    title: 'search',
-    results: results,
-  });
-}
-
-module.exports = { homeRoute, geniusRedirect, search };
+module.exports = { homeRoute };
