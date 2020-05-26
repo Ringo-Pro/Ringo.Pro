6fa8c54990407434a754079c5266b29111f3a2cf
CountNick <meijer.nick1@gmail.com>
2020-05-26 17:17:30 +0200

    Working on filter ui

--- server/static/js/filter-toggle.js
+++ server/static/js/filter-toggle.js
@@ -2,9 +2,17 @@ console.log('filter toggle!!')
 
 const filterButtons = document.querySelectorAll('.filter')
 const genreFilterFieldset = document.querySelector('.genres-filter')
+const vibesFilterFieldset = document.querySelector('.vibes-filter')
 
 console.log(filterButtons)
 
+filterButtons[1].addEventListener('click', () => {
+
+    vibesFilterFieldset.style.display = 'block'
+    genreFilterFieldset.style.display = 'none'
+})
 filterButtons[3].addEventListener('click', () => {
+
+    vibesFilterFieldset.style.display = 'none'
     genreFilterFieldset.style.display = 'block'
 })
