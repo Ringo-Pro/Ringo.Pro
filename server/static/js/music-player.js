e703d8f9d0269efec689ad63e6856cb14ae63cb1
CountNick <meijer.nick1@gmail.com>
2020-05-28 17:54:06 +0200

    Working on music player

---
+++ server/static/js/music-player.js
@@ -0,0 +1,204 @@
+console.log('This is musicplayer speaking')
+console.log('token: ', token)
+console.log('uris: ', uriList)
+
+let currentUri
+console.log('current uri to be played: ', currentUri)
+const playButtonList = document.querySelectorAll('.playButton')
+const pauseButton = document.querySelector('.pauseButton')
+const albumArt = document.querySelector('.album-art')
+const nowPlaying = document.querySelector('.nowPlaying')
+const trackProgression = document.querySelector('.progress')
+
+console.log(nowPlaying)
+
+// console.log(pauseButtonList)
+
+async function getPlayer(){
+
+    let options = {
+        headers: { Authorization: 'Bearer ' + token },
+        method: 'GET'
+      };
+
+    const fetchGetNowPlaying = await fetch('https://api.spotify.com/v1/me/player', options)
+    const nowPlaying = await fetchGetNowPlaying.json()
+
+    console.log('the async function boii: ', nowPlaying)
+
+    return nowPlaying
+
+}
+
+window.onSpotifyWebPlaybackSDKReady = () => {
+    const player = new Spotify.Player({
+        name: 'Ringo Pro player',
+        getOAuthToken: cb => {cb(token)}
+    })
+
+      // Error handling
+    player.addListener('initialization_error', ({ message }) => { console.error(message); });
+    player.addListener('authentication_error', ({ message }) => { console.error(message); });
+    player.addListener('account_error', ({ message }) => { console.error(message); });
+    player.addListener('playback_error', ({ message }) => { console.error(message); });
+
+    
+    player.getCurrentState().then(state => {
+        if(!state){
+            nowPlaying.textContent = 'User is not playing music through the Web Playback SDK'
+            console.error('User is not playing music through the Web Playback SDK')
+            return
+        }
+
+        let {
+            current_track,
+            next_tracks: [next_track]
+        } = state.track_window
+
+        console.log('Currently Playing', current_track);
+        console.log('Playing Next', next_track);
+
+        
+
+        
+
+    })
+    
+    // Playback status updates
+    player.addListener('player_state_changed', ({
+        position,
+        duration,
+        track_window: {current_track}
+    }) => {
+        console.log('Currnetly Playing: ', current_track)
+        console.log('Position in Song: ', position)
+        console.log('Duration of Song: ', duration)
+        nowPlaying.textContent = `${current_track.name} - ${current_track.artists[0].name}`
+        albumArt.src = current_track.album.images[1].url
+        
+
+        trackProgression.max = duration
+        trackProgression.value = position
+
+    });
+
+
+    
+    
+    // Ready
+    player.addListener('ready', ({ device_id }) => {
+        console.log('Ready with Device ID', device_id);
+
+
+
+        const play = async({
+            spotify_uri,
+            playerInstance: {
+                _options: {
+                    getOAuthToken,
+                    id
+                }
+            }
+        }) => {
+            
+            // console.log(await getPlayer())
+
+
+
+            // const lastPlayed = await getPlayer()
+
+            // console.log('lastPlayed: ', lastPlayed.item.uri)
+
+            // if(lastPlayed.item.uri === spotify_uri){
+                
+            //     getOAuthToken(access_token => {
+            //         fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+            //             method: 'PUT',
+            //             body: JSON.stringify({
+            //                 "context_uri": lastPlayed.item.album.uri,
+            //                 "offset": {
+            //                   "position": lastPlayed.item.track_number
+            //                 },
+            //                 "position_ms": lastPlayed.progress_ms
+            //             }),
+            //             headers: {
+            //                 'Content-Type': 'application/json',
+            //                 'Authorization': `Bearer ${access_token}`
+            //             }
+            //         })
+            //     })
+            // } else{
+                
+                            // if now playing uri is same as clicked uri continue playing
+
+            // else do this:
+
+            getOAuthToken(access_token => {
+                fetch(`https://api.spotify.com/v1/me/player/play?device_id=${id}`, {
+                    method: 'PUT',
+                    body: JSON.stringify({uris: [spotify_uri]}),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                })
+            })
+            // } 
+
+        }
+
+        
+
+        const pause = ({
+            playerInstance: {
+                _options: {
+                    getOAuthToken
+                }
+            }
+        }) => {
+            getOAuthToken(access_token => {
+                fetch(`https://api.spotify.com/v1/me/player/pause`, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${access_token}`
+                    }
+                })
+            })
+        }
+
+    
+        playButtonList.forEach((playButton) => {
+            // console.log(playButton)
+        
+            playButton.addEventListener('click', event => {
+
+                return play({
+                    playerInstance: player,
+                    spotify_uri: event.target.id,
+                })
+                // console.log(event.target.id)
+            })
+        })
+
+        
+
+            pauseButton.addEventListener('click', event => {
+                return pause({
+                    playerInstance: player
+                })
+            })
+        
+
+
+    });
+
+    // Not Ready
+    player.addListener('not_ready', ({ device_id }) => {
+        console.log('Device ID has gone offline', device_id);
+    });
+    console.log('player: ', player)
+    // Connect to the player!
+    player.connect();
+
+}
