64896076cc48c2bcfb9eb8e2b3c6745cf90504ff
Marten de Bruijn <martenbruijn@gmail.com>
2020-06-04 09:14:36 +0200

    cleaned up code

--- server/static/js/filter-menu.js
+++ server/static/js/filter-menu.js
@@ -4,22 +4,12 @@ const details = document.querySelectorAll('details'),
 const checkboxes = filtersForm.querySelectorAll('input[type="checkbox"]'),
   labels = filtersForm.querySelectorAll('label');
 let allFilters = [];
-console.log(details);
-filterSearch.oninput = function input() {
-  const value = this.value;
-  console.log(value);
-};
+
 checkboxes.forEach((item) => {
   const name = item.name,
-    mainFilter = item.getAttribute('data-main-filter'),
-    label = item.childNodes;
+    mainFilter = item.dataset.mainFilter;
   allFilters.push({ name: name, main: mainFilter, item: item });
 });
-console.log(allFilters);
-allFilters.forEach((item) => {
-  item.name.includes('gen') ? console.log(item.main) : console.log(false);
-});
-// details[0].innerText
 
 function openDetails(i) {
   details[i].setAttribute('open', true);
@@ -37,65 +27,55 @@ function checkDetails(i) {
   const check = details[i].getAttribute('open');
   return check ? true : false;
 }
-checkDetails(0);
-function _testing() {
+filterSearch.addEventListener('keyup', searchForFilter);
+function display(item) {
+  if (item.classList.contains('d-none')) {
+    item.classList.toggle('d-none');
+  }
+}
+function toggleFilter(i) {
+  allFilters[i].item.classList.toggle('d-none');
+  labels[i].classList.toggle('d-none');
+}
+function searchForFilter() {
   const val = filterSearch.value.toUpperCase();
   let toOpen = [];
   for (let i = 0, length = allFilters.length; i < length; i++) {
     let _el = allFilters[i];
     let textval = _el.name;
-    const detailsIndex = allFilters[i].main;
-
+    const detailsIndex = allFilters[i].main,
+      isFilterHidden = allFilters[i].item.classList.contains('d-none');
     if (val == '') {
+      // search input === empty
       details.forEach(function (item) {
-        const _checkboxes = item.querySelectorAll('input[type="checkbox"]');
-        const _labels = item.querySelectorAll('label');
+        const _checkboxes = item.querySelectorAll('input[type="checkbox"]'),
+          _labels = item.querySelectorAll('label');
         if (item.getAttribute('open')) {
           item.removeAttribute('open');
         }
-        _checkboxes.forEach(function (item) {
-          if (item.classList.contains('d-none')) {
-            item.classList.toggle('d-none');
-          }
-        });
-        _labels.forEach(function (item) {
-          if (item.classList.contains('d-none')) {
-            item.classList.toggle('d-none');
-          }
-        });
+        _checkboxes.forEach((item) => display(item));
+        _labels.forEach((item) => display(item));
       });
     } else if (textval.toUpperCase().indexOf(val) > -1) {
+      // search input !== checkbox value
       if (!toOpen.includes(detailsIndex)) {
         toOpen.push(detailsIndex);
       }
-      if (allFilters[i].item.classList.contains('d-none')) {
-        allFilters[i].item.classList.toggle('d-none');
-        labels[i].classList.toggle('d-none');
+      if (isFilterHidden) {
+        toggleFilter(i);
       }
     } else {
-      if (!allFilters[i].item.classList.contains('d-none')) {
-        allFilters[i].item.classList.toggle('d-none');
-        labels[i].classList.toggle('d-none');
+      // search input === checkbox value
+      if (!isFilterHidden) {
+        toggleFilter(i);
       }
     }
   }
   for (let z = 0, length = 12; z < length; z++) {
-    console.log('has');
-    console.log(z);
-    console.log(toOpen);
     if (toOpen.length !== 0) {
       toOpen.includes(`${z}`) ? openDetails(z) : closeDetails(z);
     } else {
-      details.forEach(function (item) {
-        if (item.classList.contains('d-none')) {
-          item.classList.toggle('d-none');
-        }
-      });
+      details.forEach((item) => display(item));
     }
   }
 }
-filterSearch.addEventListener('keyup', _testing);
-
-// leeg ? alles weergeven maar dicht
-// match ? weergeven en open
-// geen match ? niet weergeven
