406ba3fb6762e9c7f0977b8256810edf83d92500
Marten de Bruijn <martenbruijn@gmail.com>
2020-06-04 15:14:32 +0200

    super gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,14 @@
-const gulp = require('gulp');
-const { series, parallel } = require('gulp');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const cleanCSS = require('gulp-clean-css');
+const gulp = require('gulp'),
+  { series, parallel } = require('gulp'),
+  cleanCSS = require('gulp-clean-css'),
+  autoprefixer = require('gulp-autoprefixer'),
+  concat = require('gulp-concat'),
+  terser = require('gulp-terser'),
+  imagemin = require('gulp-imagemin');
 
 function cssReset() {
   return gulp
     .src('./server/dev/reset/reset.css')
-    .pipe(concat('reset.css'))
     .pipe(cleanCSS())
     .pipe(gulp.dest('./server/static/dist/'));
 }
@@ -15,19 +16,32 @@ function css() {
   return gulp
     .src(['./server/dev/css/themes/*.css', './server/dev/css/*.css'])
     .pipe(concat('index.css'))
-    .pipe(cleanCSS())
-    .pipe(
-      autoprefixer({
-        cascade: false,
-      })
-    )
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(autoprefixer({ cascade: false }))
     .pipe(gulp.dest('./server/static/dist/'));
 }
-
+function es() {
+  return gulp
+    .src(['./server/dev/js/*.js', './server/dev/js/modules/*.js'])
+    .pipe(terser())
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./server/static/dist/'));
+}
+function img() {
+  return gulp
+    .src('./server/dev/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./server/static/dist/img/'));
+}
 function watch() {
   gulp.watch(['./server/dev/css/*.css', './server/dev/css/themes/*.css'], css);
+  gulp.watch('./src/js/*.js', es);
 }
-const buildCSS = gulp.series(cssReset, css);
+const build = gulp.series(cssReset, css, es, img);
 
-exports.css = buildCSS;
+exports.css = css;
+exports.es = es;
+exports.img = img;
 exports.watch = watch;
+exports.build = build;
+exports.default = build;
