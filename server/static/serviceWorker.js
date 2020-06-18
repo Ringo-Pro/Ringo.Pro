4909a7c239634515454fe2b1ce1d9d6136097b78
CountNick <meijer.nick1@gmail.com>
2020-06-18 10:20:48 +0200

    Updated styling

--- server/static/serviceWorker.js
+++ server/static/serviceWorker.js
@@ -2,7 +2,23 @@ const CORE_CACHE = 1
 const CACHE_NAME = `core-cache-v${CORE_CACHE}`
 
 const urlsToCache = [
-    '/',
-    '/offline',
-    '/dist/main.css'
+    '/callback',
+    '/dist/all.js',
+    '/dist/index.css',
+    '/dist/reset.css'
 ]
+
+self.addEventListener('install', (event) => {
+    console.log('Service worker install event!');
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(cache => {
+          return cache.addAll(urlsToCache).then(() => self.skipWaiting);
+        })
+    );
+  });
+
+  self.addEventListener('activate', (event) => {
+    console.log('Servive worker activated!')
+    event.waitUntil(clients.claim())
+  })
