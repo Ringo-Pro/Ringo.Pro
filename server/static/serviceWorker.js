821861381eef0825cfdb5b00d77f1ea5ca5d218e
CountNick <meijer.nick1@gmail.com>
2020-06-18 11:06:19 +0200

    Added service workers

--- server/static/serviceWorker.js
+++ server/static/serviceWorker.js
@@ -2,7 +2,7 @@ const CORE_CACHE = 1;
 const CACHE_NAME = `core-cache-v${CORE_CACHE}`;
 
 const urlsToCache = [
-  '/callback',
+//   '/callback',
   '/dist/all.js',
   '/dist/index.css',
   '/dist/reset.css',
@@ -21,3 +21,28 @@ self.addEventListener('activate', (event) => {
   console.log('Servive worker activated!');
   event.waitUntil(clients.claim());
 });
+
+self.addEventListener('fetch', (event) => {
+    console.log('fetch event: ', event.request)
+    
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+
+          if(cachedResponse){
+            console.log("Found in cache!")
+            return cachedResponse
+          }
+
+          return fetch(event.request)
+          .then((fetchResponse) => fetchResponse)
+        //   .catch((err => {
+
+        //     const isHTMLPage = event.request.method == "GET" && event.request.headers.get("accept").includes("text/html")
+
+        //     if(isHTMLPage) return caches.match("/offline")
+
+        //   }))
+
+        })
+    )
+})
